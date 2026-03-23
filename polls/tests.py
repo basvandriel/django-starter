4b68f8ce63fe18f374e23f24be7a869398138265
Bas <contact@basvandriel.nl>
2026-03-23 20:31:16 +0100

    work

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexViewTest(TestCase):
+    def test_index_returns_200(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_content(self):
+        response = self.client.get(reverse("index"))
+        self.assertIn(b"polls index", response.content)
